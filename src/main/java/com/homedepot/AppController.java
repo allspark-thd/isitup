ecded1bec93958d03b73bf39b1331443c8765b1e
Thomas Gamble <thomas_gamble@homedepot.com>
2016-05-06 12:07:05 -0400

    spring security FTW

---
+++ src/main/java/com/homedepot/AppController.java
@@ -0,0 +1,12 @@
+package com.homedepot;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableOAuth2Sso
+@RestController
+public class AppController {
+
+
+
+}
